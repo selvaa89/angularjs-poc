cff7938a344a4aaf7fb9ad5283c251960cf1b868
Ram.M <ramanathanm@gmail.com>
2014-08-02 22:21:57 +0800

    Removed commented code Removed temp vars

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,10 @@
 'use strict';
 
 define([], function () {
-    var app = angular.module('eventsApp', ['routeResolverServices', 'ngResource', 'ngRoute']);
-    window.eventsApp = app;
-    app.config(['$routeProvider', 'routeResolverProvider', '$controllerProvider', '$compileProvider', '$filterProvider', '$provide',
+    window.eventsApp = angular.module('eventsApp', ['routeResolverServices', 'ngResource', 'ngRoute']);
+    eventsApp.config(['$routeProvider', 'routeResolverProvider', '$controllerProvider', '$compileProvider', '$filterProvider', '$provide',
         function ($routeProvider, routeResolverProvider, $controllerProvider, $compileProvider, $filterProvider, $provide) {
-            app.register =
+            eventsApp.register =
             {
                 controller: $controllerProvider.register,
                 directive: $compileProvider.directive,
@@ -24,41 +23,5 @@ define([], function () {
                 .when('/sample-directive', route.resolve('SampleDirectiveController', 'templates/SampleDirective.html'))
                 .otherwise({ redirectTo: '/events' });
         }]);
-    return app;
+    return eventsApp;
 });
-
-/*var eventsApp = angular.module("eventsApp", ['ngResource', 'ngRoute']).
-    config(function($routeProvider) {
-        $routeProvider.when('/newEvent',{
-            templateUrl: 'templates/NewEvent.html',
-            controller: 'EditEventController'
-        });
-
-        $routeProvider.when('/events', {
-            templateUrl: 'templates/EventsList.html',
-            controller: 'EventsListController'
-        });
-
-        $routeProvider.when('/event/:eventId', {
-            templateUrl: 'templates/EventDetails.html',
-            controller: 'EventController',
-            resolve: {
-                event: function($route, eventData) {
-                    return eventData.getEvent($route.current.pathParams.eventId).$promise;
-                }
-            }
-        });
-
-        $routeProvider.when('/user-profile', {
-            templateUrl: 'templates/EditProfile.html',
-            controller: 'EditProfileController'
-        });
-
-        $routeProvider.when('/sample-directive', {
-            templateUrl: 'templates/SampleDirective.html',
-            controller: 'SampleDirectiveController'
-        });
-
-        $routeProvider.otherwise({redirectTo: '/events'});
-    });
-*/
