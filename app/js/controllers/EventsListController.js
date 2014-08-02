360d755e329661fb5cb4d27b8221e4f0aae4efaa
Ram.M <ramanathanm@gmail.com>
2014-08-02 21:38:51 +0800

    Fixed merge issues

--- app/js/controllers/EventsListController.js
+++ app/js/controllers/EventsListController.js
@@ -1,5 +1,15 @@
 'use strict';
 
-eventsApp.controller('EventsListController', function($scope, eventData) {
-    $scope.events =  eventData.getAllEvents();
-})
+define(['services/EventData', 'directives/EventThumbnail', 'directives/collapsible'], function () {
+    eventsApp.controller('EventsListController', function ($scope, eventData) {
+        $scope.events = eventData.getAllEvents();
+
+        $scope.getAllEvents = function () {
+            $scope.events = eventData.getAllEvents();
+        };
+
+        $scope.navigateToDetails = function (event) {
+            $location.url('/event/' + event.id);
+        };
+    });
+});
